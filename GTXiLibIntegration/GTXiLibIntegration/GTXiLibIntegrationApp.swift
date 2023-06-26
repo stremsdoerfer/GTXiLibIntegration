30f8c433471181d06c0791975dbc89b6d54d9444
Emilien Stremsdoerfer <emstre@verily.com>
2023-06-26 15:34:23 -0700

    Initial commit

---
+++ GTXiLibIntegration/GTXiLibIntegration/GTXiLibIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GTXiLibIntegrationApp.swift
+//  GTXiLibIntegration
+//
+//  Created by Emilien Stremsdoerfer on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GTXiLibIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
